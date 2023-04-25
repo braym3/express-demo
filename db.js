77d92147be0054a09e975bae921bd11f7280fbe5
braym3 <60962768+mbray3@users.noreply.github.com>
2023-04-25 16:33:15 +0100

    Connected crud methods to mongoDB

--- db.js
+++ db.js
@@ -3,3 +3,13 @@ const mongoose = require(`mongoose`); // import mongoose
 mongoose.connect(`mongodb://127.0.0.1:27017/cats`, {}) // connect to mongo - mongo default port 27017
 .then(()=>console.log(`yay`))
 .catch(()=>console.log(`boo`))
+
+const catSchema = new mongoose.Schema({ // create the schema (the structure)
+    name: {type: String, required: true}, // string, not null
+    colour: String,
+    evil: Boolean
+}); 
+
+const catModel = mongoose.model(`cat`, catSchema) // create a model - allows you to interact with the collection - like DAO
+
+module.exports = {catModel}; // export the model
--- routes/cats.js
+++ routes/cats.js
@@ -1,41 +1,53 @@
-const router = require(`express`).Router(); // create a router, import express
 // Encapsulate routes together - creating router, giving it a bunch of routes & exporting it (declutter index)
+const router = require(`express`).Router(); // create a router, import express
+const {catModel} = require(`../db`); // import model
 
 const cats = []; // saving cats to list - no persistence
 
 // Read
-router.get(`/getAll`, (req,res)=>{ // get request - path & callback(req, res)
-    res.json(cats)
+router.get(`/getAll`, async (req, res, next)=>{ // get request - path & callback(req, res)
+    try{
+        res.json(await catModel.find({})); // return find all
+    }catch(err){
+        return next({status: 500, msg: `Oops!`});
+    }
 }); 
 
 // Create
-router.post(`/create`, (req, res)=>{
-    const newCat = req.body;
-    cats.push(newCat);
-
-    res.status(201).json(cats[cats.length-1]); // set 201 created status and return latest cat
+router.post(`/create`, async ({body}, res, next)=>{ // destructure out the body (req.body)
+    try{
+        const created = await catModel.create(body); // create cat using request body
+        res.status(201).json(created); // set 201 created status and return created cat
+    }catch(err){
+        return next({status: 500, msg: `Oops!`});
+    }
 });
 
 // Delete
-router.delete(`/remove/:id`, (req, res)=>{ // sets variable as part of path
-    const{id} = req.params; // destructuring id from params
-    const removed = cats.splice(id, 1); // index & num of elements to delete - returns deleted element
-    res.json(removed);
+router.delete(`/remove/:id`, async (req, res, next)=>{ // sets variable as part of path
+    try{
+        const {id} = req.params; // destructuring id by params
+        res.json(await catModel.findByIdAndDelete(id)); // delete cat by id and return deleted cat
+    }catch(err){
+        return next({status: 500, msg: `Oops!`});
+    }
 });
 
 // Update
-router.patch(`/update/:id`, (req, res, next)=>{
-    const{id} = req.params;
+router.patch(`/update/:id`, async (req, res, next)=>{
+    try{
+        const {id} = req.params; // destructuring id by params
 
-    // check if id is correct
-    if(id >= cats.length){ // checks if id is out of bounds
-        return next({ msg: `ID out of bounds`, status: 404}); // returns next with object of message & status code
+        // check if id is correct
+        if(id >= cats.length){ // checks if id is out of bounds
+            return next({ msg: `ID out of bounds`, status: 404}); // returns next with object of message & status code
+        }
+
+        const updatedCat = await catModel.findByIdAndUpdate(id, req.query, {returnDocument: `after`}) // update by id using query params
+        res.json(updatedCat); // return updated cat
+    }catch(err){
+        return next({status: 500, msg: `Oops!`});
     }
-
-    const{name} = req.query; // get query params
-    const catToUpdate = cats[id]; // get cat to update
-    catToUpdate.name = name; // set name to updated value
-    res.json(catToUpdate); // return updated cat
 });
 
 module.exports = router; // export router from the file
